5addc556a13cec13bb018e11ee688b29cd8e4660
AK <67439739+7086600@users.noreply.github.com>
2021-11-06 21:57:59 +0300

    move methods to package cache

--- cache/cache.go
+++ cache/cache.go
@@ -1,43 +1,61 @@
 package cache
 
 import (
+	"errors"
 	"time"
 )
 
 type Cache struct {
-	Items map[string]value
+	Items map[string]item
 }
 
-type value struct {
-	item interface{}
-	ttl  time.Duration
+type item struct {
+	value interface{}
+	ttl   int64
 }
 
 // Make new Cache
 func New() *Cache {
 	return &Cache{
-		Items: make(map[string]value),
+		Items: make(map[string]item),
+	}
+}
+
+// new item of cache
+func newItem(value interface{}, ttl time.Duration) item {
+	return item{
+		value: value,
+		ttl:   time.Now().Add(ttl).Unix(),
 	}
 }
 
 // Add cache-value to Cache
-func (c *Cache) Set(key string, val value, ttl time.Duration) {
-	c.Items[key] = val
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
+	c.Items[key] = newItem(value, ttl)
 }
 
 // Get cache-value from Cache
-func (c Cache) Get(k string) interface{} {
-	value, exists := c.Items[k]
-	if exists {
-		return value
-	} else {
-		return nil
+func (c Cache) Get(key string) (interface{}, error) {
+	c.searchingExpiredValue(key)
+	item, exists := c.Items[key]
+	if !exists {
+		return nil, errors.New("key is not exist")
 	}
-
+	return item.value, nil
 }
 
 // Delete cache-value from Cache
-func (c *Cache) Delete(k string) error {
-	delete(c.Items, k)
+func (c *Cache) Delete(key string) error {
+	delete(c.Items, key)
+	return nil
+}
+
+func (c *Cache) searchingExpiredValue(key string) error {
+	t0 := time.Now().Unix()
+	for key, item := range c.Items {
+		if t0 > item.ttl {
+			c.Delete(key)
+		}
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -1,91 +1,42 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"time"
+
+	"go.mod/cache"
 )
-
-type cachestorage struct {
-	items map[string]item
-}
-
-func newCache() *cachestorage {
-	cache := &cachestorage{
-		items: make(map[string]item),
-	}
-	return cache
-}
-
-func (c *cachestorage) set(key string, val interface{}, ttl time.Duration) {
-	v := newItem(val, ttl)
-	c.items[key] = v
-}
-
-func (c cachestorage) get(key string) (interface{}, error) {
-	c.searchingExpiredValue(key)
-	val, exists := c.items[key]
-	if !exists {
-		return nil, errors.New("key is not exist")
-	} else {
-		return val.value, nil
-	}
-}
-
-func (c *cachestorage) delete(key string) error {
-	delete(c.items, key)
-	return nil
-}
-
-func (c *cachestorage) searchingExpiredValue(key string) error {
-	t0 := time.Now().Unix()
-	for key, item := range c.items {
-		if t0 > item.ttl {
-			c.delete(key)
-		}
-	}
-	return nil
-}
-
-type item struct {
-	value interface{}
-	ttl   int64
-}
-
-func newItem(value interface{}, ttl time.Duration) item {
-	item := item{
-		value: value,
-		ttl:   time.Now().Add(ttl).Unix(),
-	}
-	return item
-}
 
 func main() {
 	fmt.Println("Hello")
 
-	cachestorage1 := newCache()
-	fmt.Printf("Пустой кэш:%v\n", cachestorage1.items)
+	cachestorage := cache.New()
+	fmt.Printf("Пустой кэш:%v\n", cachestorage.Items)
 
-	cachestorage1.set("KEY1", "Enythings", time.Second*2)
-	cachestorage1.set("KEY2", "TODAY", time.Second*7)
-	cachestorage1.set("KEY3", "YESTODAY", time.Second*10)
+	for i := 0; i < 1000; i++ {
+		cachestorage.Set(fmt.Sprintf("KEY%d", i), fmt.Sprintf("VALUE%d", i), time.Second*time.Duration(i+1))
+	}
 
-	fmt.Printf("Кэш значений:%v\n", cachestorage1.items)
+	fmt.Printf("Кол-во значений в КЭШ:%v\n", len(cachestorage.Items))
 
-	key, err := cachestorage1.get("KEY1")
+	key, err := cachestorage.Get("KEY0")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(key)
 
-	time.Sleep(time.Second * 8)
+	time.Sleep(time.Second * 2)
 
-	key, err = cachestorage1.get("KEY2")
-	if err != nil {
-		log.Fatal(err)
+	for i := 0; i < 1000; i++ {
+		key, err = cachestorage.Get(fmt.Sprintf("KEY%d", i))
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	fmt.Println(key)
+	fmt.Printf("Кол-во значений в КЭШ:%v\n", len(cachestorage.Items))
 
-	fmt.Printf("%v\n", cachestorage1)
+	// fmt.Println(key)
+
+	// fmt.Printf("%v\n", cachestorage)
 }
